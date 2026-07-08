6d157137b8d03c8bd41c81101ac553271e0d88ae
sarvesh871 <83116905+sarvesh871@users.noreply.github.com>
2026-07-08 14:54:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 with open("students.json", 'r') as f:
   students = json.load(f)
 
-  topper = max(students, key=lambda x: x["marks'"])
+  topper = max(students, key=lambda x: x["marks"])
 
   print("Topper:", topper["name"])
   print("Marks:", topper["marks"])
